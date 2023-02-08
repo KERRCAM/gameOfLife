c63c45151e0916b6933c3358d1369453b0fbbc7e
KERRCAM <90578826+KERRCAM@users.noreply.github.com>
2023-02-08 23:59:51 +0000

    variable changes of input

--- main.py
+++ main.py
@@ -11,21 +11,21 @@ pygame.init()
 pygame.display.set_caption("conway's game of life")
 
 
-def update(screen, cells, size, with_progress=False):
+def update(screen, cells, size, lonely, overpop, with_progress=False):
     updated_cells = np.zeros((cells.shape[0], cells.shape[1]))
 
     for row, col in np.ndindex(cells.shape):
-        alive = np.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col]
+        alive = np.sum(cells[row - 1:row + 2, col - 1:col + 2]) - cells[row, col]
         if cells[row, col] == 0:
             color = COLOR_BG
         else:
             color = COLOR_ALIVE_NEXT
 
         if cells[row, col] == 1:
-            if alive < 2 or alive > 3:
+            if alive < lonely or alive > overpop:  # 1 then 5 - 2 then 3
                 if with_progress:
                     color = COLOR_DIE_NEXT
-            elif 2 <= alive <= 3:
+            elif lonely <= alive <= overpop:  # 1 then 5 - 2 then 3
                 updated_cells[row, col] = 1
                 if with_progress:
                     color = COLOR_ALIVE_NEXT
@@ -40,13 +40,13 @@ def update(screen, cells, size, with_progress=False):
     return updated_cells
 
 
-def main():
+def game(lonely, overpop):
     pygame.init()
     screen = pygame.display.set_mode((1800, 1000))
 
     cells = np.zeros((100, 180))
     screen.fill(COLOR_GRID)
-    update(screen, cells, 10)
+    update(screen, cells, 10, lonely, overpop)
 
     pygame.display.flip()
     pygame.display.update()
@@ -61,21 +61,27 @@ def main():
             elif Q.type == pygame.KEYDOWN:
                 if Q.key == pygame.K_SPACE:
                     running = not running
-                    update(screen, cells, 10)
+                    update(screen, cells, 10, lonely, overpop)
                     pygame.display.update()
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
                 cells[pos[1] // 10, pos[0] // 10] = 1
-                update(screen, cells, 10)
+                update(screen, cells, 10, lonely, overpop)
                 pygame.display.update()
 
         screen.fill(COLOR_GRID)
 
         if running:
-            cells = update(screen, cells, 10, with_progress=True)
+            cells = update(screen, cells, 10, lonely, overpop, with_progress=True)
             pygame.display.update()
 
         time.sleep(0.001)
+
+
+def main():
+    lonely = int(input("what loneliness number would you like? "))
+    overpop = int(input("what overpopulation number would you like? "))
+    game(lonely, overpop)
 
 
 if __name__ == "__main__":
